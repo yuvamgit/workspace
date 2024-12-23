0c54c6f07346cfad511803aba70cc98ec1ae714c
jackaws <jackaws007@gmail.com>
2024-12-23 22:04:50 +0530

    added set code

--- For_eachMetaArg_Set-demo/main.tf
+++ For_eachMetaArg_Set-demo/main.tf
@@ -1,8 +1,8 @@
 resource "aws_iam_user" "test_user" {
-    for_each = toset(["pavan", "kumar", "ram"])
-    name = each.key
+  for_each = toset(["pavan", "kumar", "ram"])
+  name     = each.key
 
-    tags = {
-      name = each.key
-    }
+  tags = {
+    Environment = each.key
+  }
 }
