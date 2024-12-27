76d1b7bc9df39b70e62b189c4044f11db49af943
jackaws <jackaws007@gmail.com>
2024-12-27 23:40:27 +0530

    added argument validation code

--- variable-input-demo/main.tf
+++ variable-input-demo/main.tf
@@ -1,7 +1,11 @@
 variable "amiid" {
   type        = string
-  description = "amiid for ap-south-1 instances"
+  description = "amiid for ap-south-1[Mumbai] instances"
   default     = "ami-0fd05997b4dff7aac"
+    validation {
+     condition     = length(var.amiid) > 4 && substr(var.amiid, 0, 4) == "ami-"
+     error_message = "The image_id value must be a valid AMI id, starting with \"ami-\"."
+  }
 }
 
 variable "ec2count" {
@@ -10,11 +14,28 @@ variable "ec2count" {
   default     = 1
 }
 
+/*
+
+variable "ec2count" {     # this block used to get count in terraform plan/apply prompt.
+}                         # very useful when there is no default value 
+
+*/
+
+variable "az" {
+  type = string
+  default = "ap-south-1a"
+  description = "AWS availability zone"
+    validation {
+      condition = contains(["ap-south-1a", "ap-south-1b"], var.az)
+      error_message = "Pleas specify correct availability zone from ap-south-1a or ap-south-1b"
+    } 
+}
+
 resource "aws_instance" "demo-server" {
   ami               = var.amiid
   instance_type     = "t2.micro"
   count             = var.ec2count
-  availability_zone = "ap-south-1a"
+  availability_zone = var.az
   tags = {
     Name = "demo-server"
   }
